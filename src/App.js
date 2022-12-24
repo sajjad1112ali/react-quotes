bc870c5197e3d427e60d727f39b45c2560eb5e8c
Sajjad Ali <sajjad.ali@arpatech.com>
2022-12-24 22:58:44 +0500

    Added basename

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Login from "./pages/Login/Login";
 function App() {
   return (
     <Provider store={store}>
-      <Routes>
+      <Routes basename='/react-quotes'>
         <Route path="/" element={<PublicLayout />}>
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
