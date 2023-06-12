ed78c6385e94a6460a59b8d8232e0ae99e91301b
sajjad1112ali <sajjadramzan1211@gmail.com>
2023-06-12 13:24:30 +0500

    Updated api  url

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const apisURL =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:4000/v1"
-    : "https://my-apis.bitandpixelhub.com/v1";
+    : "https://node.apis.bitandpixelhub.com/v1";
 
 module.exports = {
   APP_NAME: "Quote App",
